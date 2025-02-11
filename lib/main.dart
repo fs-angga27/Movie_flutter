55f3cf6fa27e44fd586f8083a8f1c7ec9bbc01af
fxdagx27 <fldiangga27@gmail.com>
2025-02-11 09:28:26 +0700

    seccond com

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,10 @@ import 'package:movie_flutter/views/register_user_view.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     initialRoute: '/',
     routes: {
-      '/':(context)=>RegisterUserView(),
+      '/': (context) => RegisterUserView(),
     },
   ));
 }
-
-
